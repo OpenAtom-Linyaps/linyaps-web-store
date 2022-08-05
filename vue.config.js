3fc7e3a7a629f84f59bacd23f9fb1dbd1d17f8ce
yuanqiliang <yuanqiliang@uniontech.com>
2022-08-06 00:07:51 +0800

    fix:修复网页商店标题

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   chainWebpack: config => {
     config.plugin('html').tap(args => {
-      args[0].title = 'Linglong Space';
+      args[0].title = 'Linglong Web Store';
       return args;
     });
   },
