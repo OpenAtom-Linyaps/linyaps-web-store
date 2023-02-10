a78372782ea85c5a6444e274eed8f3aa3f583690
wurongjie <wurongjie@deepin.org>
2023-02-10 16:01:50 +0800

    chore: Clean up unused files
    清理不再使用的配置文件和图片资源
    
    Log:

--- .doraemon/build/build-for-proxy.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-
-tag=${1:?}
-
-NODE_OPTIONS=--openssl-legacy-provider yarn build --mode proxy
-docker buildx build --platform linux/amd64 --pull --push -t "hub.deepin.com/wuhan_v23_linglong/linglong-web-store:${tag}" .
